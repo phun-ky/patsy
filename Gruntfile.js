0ccbe12e834885cd6ad3a1e30381f9be1628eee1
Alexander Vassbotn Røyne-Helgesen <qe1@storebrand.no>
2013-01-04 08:29:10 +0100

    Added inline comments for documentation, created new placeholder variables for bette readability

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,13 @@
-/*
- * grunt
- * https://github.com/cowboy/grunt
+/**
+ * This file holds the grunt configuration for patsy
+ *
+ * If the documented code here does not fullfill your needs, check the wiki on github here: https://github.com/phun-ky/patsy/wiki
+ *
+ * We do a bit more tweaking that we should here, but while we wait for grunt to be omnipotent, we do it like we do.
+ * 
+ * grunt is copyrighted to "Cowboy" Ben Alman
+ * 
+ * https://github.com/gruntjs/grunt
  *
  * Copyright (c) 2012 "Cowboy" Ben Alman
  * Licensed under the MIT license.
@@ -8,65 +15,144 @@
  */
  "use strict";
 
-
-// Prepare node plugin variables
+/**
+ * Require util plugin from node
+ *
+ * @var     Object
+ * @source  NodeJS
+ */
 var util              = require('util');
+
+/**
+ * Require path plugin from node
+ *
+ * @var     Object
+ * @source  NodeJS
+ */
 var path              = require('path');
 
-// Prepare patsyHelpers variable
+/**
+ * Require patsyHelpers from the library
+ *
+ * @var     Object
+ * @source  patsy
+ */
 var patsyHelpers      = require('./lib/patsyHelpers');
 
-// Prepare local variables for this file
+/**
+ * Varholder for the project name
+ *
+ * @var     String
+ * @source  patsy.json
+ */
 var project           = '';
+
+/**
+ * Varholder for the [full] project path
+ *
+ * @var     String
+ * @source  grunt.option
+ */
 var projectPath       = '';
-var _mustachePostfix  = '';
-var _mustachePrefix   = '';
 
-// Set up GruntJS
-module.exports = function(grunt) {
+/**
+ * Postfix for templating
+ *
+ * @var     String
+ * @source  patsy.json
+ */
+var templatePostfix  = '';
 
-  /**
-   * Variable for the project configuration
-   *
-   * @var     Object
-   * @source  patsy.json
-   */
-  var _projectConfig;
+/**
+ * Prefix for templating 
+ *
+ * @var     String
+ * @source  patsy.json
+ */
+var templatePrefix   = '';
 
-  var _pathToJavaScriptFiles;
+/**
+ * Variable for the project configuration
+ *
+ * @var     Object
+ * @source  patsy.json
+ */
+var projectConfig;
+
+/**
+ * Varholder for path to files for better readability
+ *
+ * @var     String
+ * @source  patsy.json
+ */
+var pathToJavaScriptFiles;
+
+/**
+ * Varholder for path to files for better readability
+ *
+ * @var     String
+ * @source  patsy.json
+ */
+var pathToTemplateFiles
+
+/**
+ * Varholder for path to files for better readability
+ *
+ * @var     String
+ * @source  patsy.json
+ */
+var pathToMinifiedFiles
+
+/**
+ * Varholder for path to files for better readability
+ *
+ * @var     String
+ * @source  patsy.json
+ */
+var pathToBakedFiles;
+
+/**
+ * Set up grunt and export it for use
+ *
+ * @var     Function 
+ */
+module.exports = function(grunt) {    
 
   // Populate project variables, used for better readability
   projectPath       = grunt.option('path');  
   
+  // Do we have a projectPath defined
   if(typeof projectPath !== 'undefined'){
 
     // Get config options from project if available
     try {
 
-        _projectConfig = patsyHelpers.loadPatsyConfigInCurrentProject(projectPath);
-        
-        if(typeof _projectConfig !== 'undefined'){
+      // Set projectConfig from patsy.json        
+      projectConfig = patsyHelpers.loadPatsyConfigInCurrentProject(projectPath);
+      
+      if(typeof projectConfig !== 'undefined'){
 
-          if( typeof _projectConfig.templatePrefix !== 'undefined' && 
-              typeof _projectConfig.templatePostfix !== 'undefined'
-          ){
+        if( typeof projectConfig.templatePrefix !== 'undefined' && 
+            typeof projectConfig.templatePostfix !== 'undefined'
+        ){
 
-            _mustachePostfix  = _projectConfig.templatePostfix;
-            _mustachePrefix   = _projectConfig.templatePrefix;
-          }
+          templatePostfix  = projectConfig.templatePostfix;
+          templatePrefix   = projectConfig.templatePrefix;
+        }
 
-          if(typeof _projectConfig.nameOfProject !== 'undefined'){
+        // Try to set project name, if not found, use the folder name
+        if(typeof projectConfig.nameOfProject !== 'undefined'){
 
-            project           = _projectConfig.nameOfProject;
-          } else {
-
-            project           = path.basename(projectPath);
-          }
+          project           = projectConfig.nameOfProject;
         } else {
 
-          console.log('Project configuration not found, exiting...');
-          process.exit(1);    
+          project           = path.basename(projectPath);
         }
+      } else {
+
+        console.log('Project configuration not found, exiting...');
+        process.exit(1);    
+      }
 
     } catch(e){
 
@@ -79,7 +165,6 @@ module.exports = function(grunt) {
     process.exit(1);
   }
 
-
   //grunt.loadNpmTasks('grunt-dox');   
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -88,7 +173,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-minified');
   //grunt.loadNpmTasks('grunt-recess'); 
 
-  _pathToJavaScriptFiles = projectPath + _projectConfig.pathToJavaScriptFiles;
+  pathToJavaScriptFiles = projectPath + projectConfig.pathToJavaScriptFiles;
 
   // GruntJS configuration.
   grunt.initConfig({
@@ -98,8 +183,8 @@ module.exports = function(grunt) {
       scripts : {
         files : [ 
                     
-          _pathToJavaScriptFiles + '**/*.js',                          
-          projectPath + _projectConfig.pathToTemplateFiles + '*.mustache'
+          pathToJavaScriptFiles + '**/*.js',                          
+          projectPath + projectConfig.pathToTemplateFiles + '*.mustache'
                     
         ], 
         tasks: ['jshint','mustache', 'minified'],
@@ -109,23 +194,23 @@ module.exports = function(grunt) {
         
       },      
       concatinate : {
-        files : [projectPath + _projectConfig.pathToMinifiedFiles + '*.js'],
+        files : [projectPath + projectConfig.pathToMinifiedFiles + '*.js'],
         tasks : ['concat']
       }
     },
     clean: {
-      folder: projectPath + _projectConfig.pathToBakedFiles + "debug/*"
+      folder: projectPath + projectConfig.pathToBakedFiles + "debug/*"
     },
     test: {
-      all: [_pathToJavaScriptFiles + 'test/**/*.js']
+      all: [pathToJavaScriptFiles + 'test/**/*.js']
     },
     minified : {
       files: {
         src: [
-          _pathToJavaScriptFiles + '**' + path.sep + '*.js',                
-          _pathToJavaScriptFiles + '*.js'
+          pathToJavaScriptFiles + '**' + path.sep + '*.js',                
+          pathToJavaScriptFiles + '*.js'
         ],
-        dest: projectPath + _projectConfig.pathToMinifiedFiles
+        dest: projectPath + projectConfig.pathToMinifiedFiles
       }
     },
     jshint : {
@@ -135,25 +220,25 @@ module.exports = function(grunt) {
         passfail: true
       },
       src: [ 
-        _pathToJavaScriptFiles + '**' + path.sep + '*.js',
-        '!' + _pathToJavaScriptFiles + 'templates.js'
+        pathToJavaScriptFiles + '**' + path.sep + '*.js',
+        '!' + pathToJavaScriptFiles + 'templates.js'
       ]
     },
     concat: {
       dist: {
         src:  [
-                projectPath + _projectConfig.pathToMinifiedFiles + '*.js'
+                projectPath + projectConfig.pathToMinifiedFiles + '*.js'
               ],
-        dest: projectPath + _projectConfig.pathToBakedFiles + project + '.core.js'
+        dest: projectPath + projectConfig.pathToBakedFiles + project + '.core.js'
       }
     },        
     mustache:{
       files: {
-        dest : _pathToJavaScriptFiles + 'templates.js',
-        src : [projectPath + _projectConfig.pathToTemplateFiles],
+        dest : pathToJavaScriptFiles + 'templates.js',
+        src : [projectPath + projectConfig.pathToTemplateFiles],
         options: {
-          postfix: typeof _mustachePostfix !== 'undefined' ? _mustachePostfix : '',
-          prefix:  typeof _mustachePrefix !== 'undefined' ? _mustachePrefix : ''
+          postfix: typeof templatePostfix !== 'undefined' ? templatePostfix : '',
+          prefix:  typeof templatePrefix !== 'undefined' ? templatePrefix : ''
         }
       }
     },    
