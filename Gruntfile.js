3383b1f843e667677f01adcc6a87782f55bf761e
Alexander Vassbotn Røyne-Helgesen <alexander@phun-ky.net>
2013-03-15 11:03:54 +0100

    for merging to the master

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,7 +130,9 @@ module.exports = function(grunt) {
 
       if(config.build.test.suites.qunit){
         testTasks.push('qunit');
-        config.build.test.suites.qunit.src = patsy.updateRelativePaths(config.project.environment.rel_path, config.build.test.suites.qunit.src);
+        if(typeof config.build.test.suites.qunit.src !== 'undefined'){
+          config.build.test.suites.qunit.src = patsy.updateRelativePaths(config.project.environment.rel_path, config.build.test.suites.qunit.src);
+        }
         grunt.loadNpmTasks('grunt-contrib-qunit');
       }
 
